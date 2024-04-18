43483a4fc0acc47249fe8395ad21b3184cd3c6f4
sam-evans <wmpsamuelevans@gmail.com>
2024-04-18 12:10:16 -0500

    Updated formated

--- General Concepts/TriangleIntegers.cpp
+++ General Concepts/TriangleIntegers.cpp
@@ -1,6 +1,11 @@
-//test g++ installed correctly and can build hello program
 #include <iostream>
-//line length of 8 chars
+/*
+Output
+       1
+     2 3
+   4 5 6
+7 8 9 10
+*/
 int main()
 {
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
